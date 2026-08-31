fd104423792338101a05ced71f4df5e41c7ff7bb
RomanVark <jrromero@uamv.edu.ni>
2026-08-31 16:41:37 -0600

    Pulperia controller

---
+++ src/main/java/ni/edu/uam/eventosynavegacion/controllers/Pulperia.java
@@ -0,0 +1,21 @@
+package ni.edu.uam.eventosynavegacion.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import ni.edu.uam.eventosynavegacion.modelos.Producto;
+
+public class Pulperia {
+    @FXML
+    private TextField txtCodigo;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtPrecio;
+    @FXML
+    private TextField txtCantidad;
+    @FXML
+    private Label lblResultado;
+
+
+}

1a7b6a449dd7c73bb98327b245f118ae7bdb6af1
RomanVark <jrromero@uamv.edu.ni>
2026-08-31 16:46:38 -0600

    Pulperia controller

--- src/main/java/ni/edu/uam/eventosynavegacion/controllers/Pulperia.java
+++ src/main/java/ni/edu/uam/eventosynavegacion/controllers/Pulperia.java
@@ -1,8 +1,10 @@
 package ni.edu.uam.eventosynavegacion.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import ni.edu.uam.eventosynavegacion.dao.ProductoDao;
 import ni.edu.uam.eventosynavegacion.modelos.Producto;
 
 public class Pulperia {
@@ -16,6 +18,16 @@ public class Pulperia {
     private TextField txtCantidad;
     @FXML
     private Label lblResultado;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private Button btnBuscar;
+
+    private final ProductoDao productoDao = new ProductoDao();
+
+    @FXML
+    private void guardarProducto(ActionEvent event) {}
+
 
 
 }

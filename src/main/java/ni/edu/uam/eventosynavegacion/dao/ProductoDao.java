164481af4cb0f7e365769311b5e0a16631bd1b75
RomanVark <jrromero@uamv.edu.ni>
2026-08-31 16:33:54 -0600

    Dao terminado

--- src/main/java/ni/edu/uam/eventosynavegacion/dao/ProductoDao.java
+++ src/main/java/ni/edu/uam/eventosynavegacion/dao/ProductoDao.java
@@ -3,8 +3,41 @@ package ni.edu.uam.eventosynavegacion.dao;
 import ni.edu.uam.eventosynavegacion.Interface.Crud;
 import ni.edu.uam.eventosynavegacion.modelos.Producto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductoDao implements Crud<Producto> {
 
+    private final List<Producto> productos = new ArrayList<>();
 
+
+    @Override
+    public void agregar(Producto producto) {
+        productos.add(producto);
+
+    }
+
+    @Override
+    public Producto Buscar(String valor) {
+        for (Producto producto : productos) {
+            if (producto.getCodigo().equals(valor)) {
+                return producto;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Producto> ObtenerRegistros() {
+        return productos;
+    }
+    public Producto buscarporcodigo(String codigo) {
+        for (Producto producto : productos) {
+            if ( producto.getCodigo().equals(codigo)) {
+                return producto;
+            }
+        }
+        return null;
+    }
 }
 

e22070cfccbab74b54606994009dcc45c3e719b4
BettleJuice-cmd <asdiazm@uamv.edu.ni>
2026-08-31 16:34:33 -0600

    Haciendo avances

---
+++ src/main/java/ni/edu/uam/eventosynavegacion/controllers/LoteController.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.eventosynavegacion.controllers;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class LoteController {
+
+    @FXML
+    private TextField txtCode;
+    @FXML
+    private
+}

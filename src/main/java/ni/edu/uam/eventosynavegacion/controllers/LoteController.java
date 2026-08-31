29f980bcdf4e31a1180a20c79df12b91ae93988e
BettleJuice-cmd <asdiazm@uamv.edu.ni>
2026-08-31 16:47:38 -0600

    Haciendo avances

--- src/main/java/ni/edu/uam/eventosynavegacion/controllers/LoteController.java
+++ src/main/java/ni/edu/uam/eventosynavegacion/controllers/LoteController.java
@@ -1,6 +1,11 @@
 package ni.edu.uam.eventosynavegacion.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import ni.edu.uam.eventosynavegacion.modelos.Lote;
 
 import java.awt.*;
 
@@ -9,5 +14,65 @@ public class LoteController {
     @FXML
     private TextField txtCode;
     @FXML
-    private
+    private TextField txtProductor;
+    @FXML
+    private TextField txtWeight;
+    @FXML
+    private TextField txtEntrega;
+    @FXML
+    private Button btnRegistrar;
+    @FXML
+    private TableView<Lote> tvRegistros;
+
+    @FXML
+    private TableColumn<Lote, String> colCodigo;
+
+    @FXML
+    private TableColumn<Lote, String> colProductor;
+
+    @FXML
+    private TableColumn<Lote, Double> colPeso;
+
+    @FXML
+    private TableColumn<Lote, String> colFecha;
+
+    @FXML
+    private TextArea txtDetalles;
+
+    @FXML
+    public void initialize() {
+        colCodigo.setCellValueFactory(
+                new PropertyValueFactory<>("codigo")
+        );
+
+        colProductor.setCellValueFactory(
+                new PropertyValueFactory<>("productor")
+        );
+
+        colPeso.setCellValueFactory(
+                new PropertyValueFactory<>("peso")
+        );
+
+        colFecha.setCellValueFactory(
+                new PropertyValueFactory<>("fecha")
+        );
+
+        // MouseEvent
+        tvRegistros.setOnMouseClicked(this::mostrarDetalles);
+
+
+        // ContextMenu
+        ContextMenu menuContextual = new ContextMenu();
+
+        MenuItem opcionEditar = new MenuItem("Editar");
+        MenuItem opcionEliminar = new MenuItem("Eliminar");
+
+        menuContextual.getItems().addAll(opcionEditar, opcionEliminar);
+
+
+        //Evento Editar
+        opcionEditar.setOnAction
+
+    }
+
 }
